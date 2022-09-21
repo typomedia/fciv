68d80e758afdc1fbf6155fbdd4d689b93182b9bb
Philipp Speck <philipp@typo.media>
2022-09-22 00:27:19 +0200

    test added

---
+++ tests/Converter/CamelCaseToUpperCaseConverterTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Typomedia\Fciv\Tests\Converter;
+
+use Typomedia\Fciv\Converter\CamelCaseToUpperCaseConverter;
+use PHPUnit\Framework\TestCase;
+
+class CamelCaseToUpperCaseConverterTest extends TestCase
+{
+    public function testDenormalize()
+    {
+        $converter = new CamelCaseToUpperCaseConverter();
+
+        $this->assertEquals('fileEntry', $converter->denormalize('FILE_ENTRY'));
+
+    }
+
+    public function testDenormalizeLong()
+    {
+        $converter = new CamelCaseToUpperCaseConverter();
+
+        $this->assertEquals('fileEntryLong', $converter->denormalize('FILE_ENTRY_LONG'));
+
+    }
+
+    public function testNormalize()
+    {
+        $converter = new CamelCaseToUpperCaseConverter();
+
+        $this->assertEquals('FILE_ENTRY', $converter->normalize('fileEntry'));
+
+    }
+
+    public function testNormalizeLong()
+    {
+        $converter = new CamelCaseToUpperCaseConverter();
+
+        $this->assertEquals('FILE_ENTRY_LONG', $converter->normalize('fileEntryLong'));
+
+    }
+}
