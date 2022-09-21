00ea8600cd3bca705027aa127061278732a6b9d0
Philipp Speck <philipp@typo.media>
2022-09-22 00:05:50 +0200

    several changes

--- tests/Validator/VerifierTest.php
+++ tests/Validator/VerifierTest.php
@@ -10,7 +10,7 @@ class VerifierTest extends TestCase
     /**
      * @throws \Exception
      */
-    public function testVerifySimple()
+    public function testVerifySingle()
     {
         $input = __DIR__ . '/../Fixtures/test.xml';
 
